3e53089e34b775a1adf10459a0d312d93368c1c9
Tom King <mobilemind@pobox.com>
2012-08-18 08:21:57 -0700

    web/*.js: rebuilt

--- web/fyi-ie.js
+++ web/fyi-ie.js
@@ -1,1 +1,1 @@
-javascript:var%20r='%250A',t=encodeURIComponent(document.title),g=document.selection;location.href='mailto:?subject=fyi:%20'+t+'&body='+t+r+location.href+r+(g&&g.createRange().text?'---'+r+encodeURIComponent(g.createRange().text)+r:'')+r+r;void('2.3ie')
+javascript:var%20r='%250A',t=encodeURIComponent(document.title),g=document.selection;location.href='mailto:?subject=fyi:%20'+t+'&body='+t+r+location.href+r+(g&&g.createRange().text?'---'+r+encodeURIComponent(g.createRange().text)+r+r:r);void'2.4ie'
