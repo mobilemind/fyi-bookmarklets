2919d488d89b886fdc64edda483ef195d231158b
Tom King <mobilemind@pobox.com>
2013-12-29 16:47:05 -0800

    Gruntfile.js: flattent config items files; use grunt.config(property) instead of a var

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,13 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       firefox: {
-        src: ['src/fyi-firefox.js'],
-        dest: 'web/fyi-firefox.js'
+        src: ['src/fyi-firefox.js'], dest: 'web/fyi-firefox.js'
       },
       ie: {
-        src: ['src/fyi-ie.js'],
-        dest: 'web/fyi-ie.js'
+        src: ['src/fyi-ie.js'], dest: 'web/fyi-ie.js'
       },
       webkit: {
-        src: ['src/fyi-webkit.js'],
-        dest: 'web/fyi-webkit.js'
+        src: ['src/fyi-webkit.js'], dest: 'web/fyi-webkit.js'
       },
       options: {
         compress: {
@@ -69,34 +66,23 @@ module.exports = function(grunt) {
         appendVersion: true
       },
       firefox: {
-        src: ['web/fyi-firefox.js'],
-        dest: 'web/fyi-firefox.js'
+        src: ['web/fyi-firefox.js'], dest: 'web/fyi-firefox.js'
       },
       ie: {
-        src: ['web/fyi-ie.js'],
-        dest: 'web/fyi-ie.js'
+        src: ['web/fyi-ie.js'], dest: 'web/fyi-ie.js'
       },
       webkit: {
-        src: ['web/fyi-webkit.js'],
-        dest: 'web/fyi-webkit.js'
+        src: ['web/fyi-webkit.js'], dest: 'web/fyi-webkit.js'
       }
     }
   });
 
-  // Load "jshint" plugin
+  // Load plugins
   grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  // Load "uglify" plugin
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Load "js2uri" plugin
   grunt.loadNpmTasks('js2uri');
 
-  // default version to metadata version OR pkg.version if available
-  var fyi_pkgVersion = '';
-  if (undefined !== grunt.config('meta.version')) fyi_pkgVersion = grunt.config('meta.version');
-  else if (undefined !== grunt.config('pkg.version')) fyi_pkgVersion = grunt.config('pkg.version');
-  grunt.log.writeln('\n' + grunt.config('pkg.name') + ' ' + fyi_pkgVersion);
+  grunt.log.writeln('\n' + grunt.config('pkg.name') + ' ' + grunt.config('pkg.version'));
 
   grunt.registerTask('firefox', 'process firefox', function() {
     grunt.task.run(['uglify:firefox', 'js2uri:firefox', 'post-firefox']);
@@ -104,7 +90,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('post-firefox', 'post-process firefox', function() {
     var jsString = grunt.file.read('web/fyi-firefox.js').replace(':%25s?', ':%s?');
-    jsString = jsString.replace(fyi_pkgVersion, fyi_pkgVersion + 'ff');
+    jsString = jsString.replace(grunt.config('pkg.version'), grunt.config('pkg.version') + 'ff');
     grunt.file.write('web/fyi-firefox.js', jsString);
     grunt.log.writeln('web/fyi-firefox.js (' + jsString.length + ' bytes)');
   });
@@ -114,7 +100,7 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('post-ie', 'post-process IE', function() {
-		var jsString = grunt.file.read('web/fyi-ie.js').replace(fyi_pkgVersion, fyi_pkgVersion + "ie");
+		var jsString = grunt.file.read('web/fyi-ie.js').replace(grunt.config('pkg.version'), grunt.config('pkg.version') + "ie");
     grunt.file.write('web/fyi-ie.js', jsString);
     grunt.log.writeln('web/fyi-ie.js (' + jsString.length + ' bytes)');
   });
@@ -124,7 +110,7 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('post-webkit', 'post-process WebKit', function() {
-		var jsString = grunt.file.read('web/fyi-webkit.js').replace(fyi_pkgVersion, fyi_pkgVersion + "wk");
+		var jsString = grunt.file.read('web/fyi-webkit.js').replace(grunt.config('pkg.version'), grunt.config('pkg.version') + "wk");
     grunt.file.write('web/fyi-webkit.js', jsString);
     grunt.log.writeln('web/fyi-webkit.js (' + jsString.length + ' bytes)');
   });
