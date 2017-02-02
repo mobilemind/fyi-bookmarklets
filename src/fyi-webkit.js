7d4cd8b9e8b564af219dd378282d878b493f828d
Tom King <mobilemind@pobox.com>
2017-02-02 15:20:03 -0800

    src/*.js: use curly braces with "if" clauses

--- src/fyi-webkit.js
+++ src/fyi-webkit.js
@@ -4,4 +4,6 @@ var defaultEmail = 'user@domain.tld',
 	pageTitle = encodeURIComponent(document.title),
 	selectedText = window.getSelection(),
 	emailAddress = window.prompt('Send link to email address(es):', defaultEmail);
-	if ('' !== emailAddress) location.href = 'mailto:' + emailAddress + '?subject=fyi: ' + pageTitle + '&body=' + pageTitle + returnChar + location.href + returnChar + (selectedText ? '---' + returnChar + encodeURIComponent(selectedText) + returnChar + returnChar : returnChar);
+	if ('' !== emailAddress) {
+		location.href = 'mailto:' + emailAddress + '?subject=fyi: ' + pageTitle + '&body=' + pageTitle + returnChar + location.href + returnChar + (selectedText ? '---' + returnChar + encodeURIComponent(selectedText) + returnChar + returnChar : returnChar);
+	}
