b9532ecdd2e7c11b345534f088815e906420ebb9
Tom King <mobilemind@pobox.com>
2017-02-02 14:19:26 -0800

    src/*.js: update based on feedback from Codacy

--- src/fyi-firefox.js
+++ src/fyi-firefox.js
@@ -4,5 +4,9 @@ var defaultEmail = 'user@domain.tld',
 	pageTitle = encodeURIComponent(document.title),
 	selectedText = window.getSelection(),
 	emailAddress = '%s'; // '%s' is the argument string Firefox can provide to bookmark(lets)
-	if (emailAddress.length < 3) emailAddress = window.prompt('Send link to email address(es):', defaultEmail);
-	if ('' !== emailAddress) location.href = 'mailto:' + emailAddress + '?subject=fyi: ' + pageTitle + '&body=' + pageTitle + returnChar + location.href + returnChar + (selectedText ? '---' + returnChar + encodeURIComponent(selectedText) + returnChar + returnChar : returnChar);
+	if (emailAddress.length < 3) {
+		emailAddress = window.prompt('Send link to email address(es):', defaultEmail);
+	}
+	if ('' !== emailAddress) {
+		location.href = 'mailto:' + emailAddress + '?subject=fyi: ' + pageTitle + '&body=' + pageTitle + returnChar + location.href + returnChar + (selectedText ? '---' + returnChar + encodeURIComponent(selectedText) + returnChar + returnChar : returnChar);
+	}
--- src/fyi-ie.js
+++ src/fyi-ie.js
@@ -4,4 +4,4 @@ var defaultEmail = 'user@domain.tld',
 	pageTitle = encodeURIComponent(document.title),
 	selectedText = document.selection,
 	emailAddress = window.prompt('Send link to email address(es):', defaultEmail);
-	if ('' !== emailAddress) location.href = 'mailto:' + emailAddress + '?subject=fyi: ' + pageTitle + '&body=' + pageTitle + returnChar + location.href + returnChar + (selectedText && selectedText.createRange().text ? '---' + returnChar + encodeURIComponent(selectedText.createRange().text) + returnChar  + returnChar : returnChar);
+	if ('' !== emailAddress) location.href = 'mailto:' + emailAddress + '?subject=fyi: ' + pageTitle + '&body=' + pageTitle + returnChar + location.href + returnChar + (selectedText && selectedText.createRange().text ? '---' + returnChar + encodeURIComponent(selectedText.createRange().text) + returnChar + returnChar : returnChar);
