f357c4b8c23f1c0d7eec830b141d6fc37d9ebc2d
Tom King <mobilemind@pobox.com>
2022-04-12 23:08:03 -0700

    Gruntfile.js: use subshell to finally resolve PIPE error issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         },
         "pkg": grunt.file.readJSON("package.json"),
         "shell": {
-            "mkdir_web": {"command": "(test -d 'web' || mkdir 'web')"},
+            "mkdir_web": {"command": "(mkdir 'web' || true)"},
             "uglify_es": {"command": "for FYIJS in src/fyi-*.js; do uglifyjs --config-file .uglifyjs3.json --output \"web/$(basename \"$FYIJS\")\" \"$FYIJS\" ; done"}
         }
     });
