a92807ced02657eb6cbcc171e77d46edba30c0c0
Tom King <mobilemind@pobox.com>
2023-12-26 11:30:52 -0800

    safer URL encoding; modernize build & lint

---
+++ .github/linters/.markdownlint.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "config": {
+          "default": true,
+          "blanks-around-lists": false,
+          "no-inline-html": { "allowed_elements": "a" },
+          "no-bare-urls": false,
+      }
+}
