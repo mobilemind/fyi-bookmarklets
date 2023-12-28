979752c5f7e6ac38171339a821052cabdbcef2a5
Tom King <mobilemind@users.noreply.github.com>
2023-12-28 23:20:54 +0000

    update CodeSandbox tasks, preflight, & README.md

--- .github/linters/.markdownlint.js
+++ .github/linters/.markdownlint.js
@@ -2,6 +2,7 @@ module.exports = {
     "config": {
           "default": true,
           "blanks-around-lists": false,
+          "line-length": false,
           "no-inline-html": { "allowed_elements": "a" },
           "no-bare-urls": false,
       }
